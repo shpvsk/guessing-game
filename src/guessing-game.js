f801bff2d1baf2392c17307c0b7809a917131396
shpvsk <mshpvsk@yandex.ru>
2022-08-16 18:22:09 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
+    min
+    mid
+    max
+
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.mid = Math.ceil((this.min + this.max) / 2)
+        return this.mid
     }
 
     lower() {
-
+        this.max = this.mid
+        return this.max = this.mid
     }
 
     greater() {
-
+        this.min = this.mid
+        return this.min = this.mid
     }
 }
 
